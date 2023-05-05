b9fe89d190ab9824a4844074f10e19d6fab135b0
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-05 14:45:49 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ function Home() {
 
 function About(){
   return(
-    <div id ="about">Think <b>Twice</b> Code <b>Once</b></div>
+    <div id ="about">Kenya <b>Africa</b> Country <b>Once</b></div>
   );
 }
 
